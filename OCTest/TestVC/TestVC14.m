36b08b9efc3f9ad7732a96d7c7a52365253f7d85
xiaohuiCoding <2384618767@qq.com>
2018-03-26 10:51:32 +0800

    clean

--- OCTest/TestVC/TestVC14.m
+++ OCTest/TestVC/TestVC14.m
@@ -11,10 +11,6 @@
 #import "TestVC14.h"
 #import <mach/mach.h>
 
-#define DISPATCH_ON_MAIN_THREAD(mainQueueBlock) dispatch_async(dispatch_get_main_queue(), (mainQueueBlock));
-
-#define SCREEN_WIDTH [[UIScreen mainScreen] bounds].size.width
-
 static const int kStep = 100000;
 static const int kIterationCount = 10 * kStep;
 
@@ -31,7 +27,6 @@ static const int kIterationCount = 10 * kStep;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    //Init
     _memoryUsageList1 = [NSMutableArray new];
     _memoryUsageList2 = [NSMutableArray new];
 }
@@ -39,14 +34,9 @@ static const int kIterationCount = 10 * kStep;
 - (IBAction)test:(UIButton *)sender {
 
     sender.enabled = NO;
-    [UIView animateWithDuration:0.5f animations:^{
-        sender.alpha = 0.1;
-    }];
-    
-    
+
     //创建串行队列
     dispatch_queue_t serialQueue = dispatch_queue_create("me.tutuge.test.autoreleasepool", DISPATCH_QUEUE_SERIAL);
-    
     
     //使用自动释放池
     dispatch_sync(serialQueue, ^{
@@ -82,19 +72,18 @@ static const int kIterationCount = 10 * kStep;
     
     //测试完成
     dispatch_sync(serialQueue, ^{
-        DISPATCH_ON_MAIN_THREAD(^{
+        dispatch_async(dispatch_get_main_queue(), ^{
             for (NSInteger i = 0; i < _memoryUsageList1.count; i++) {
                 NSLog(@"使用自动释放池：%@",_memoryUsageList1[i]);
             }
             for (NSInteger i = 0; i < _memoryUsageList2.count; i++) {
                 NSLog(@"不使用自动释放池：%@",_memoryUsageList2[i]);
             }
-        })
+        });
     });
 }
 
-#pragma mark - Memory methods
-
+//获取内存
 double getMemoryUsage(void) {
     struct task_basic_info info;
     mach_msg_type_number_t size = sizeof(info);
