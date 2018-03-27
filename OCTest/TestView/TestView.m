8b73ef5aacd2ea06b122152f6bc10f305ac61562
xiaohuiCoding <2384618767@qq.com>
2018-03-27 16:00:26 +0800

    update

--- OCTest/TestView/TestView.m
+++ OCTest/TestView/TestView.m
@@ -17,18 +17,18 @@
         return nil;
     }
     //如果事件发生在父视图里就返回
-    UIView *result = [super hitTest:point withEvent:event];
-    if (result) {
-        return result;
+    UIView *responseView = [super hitTest:point withEvent:event];
+    if (responseView) {
+        return responseView;
     }
     //如果不在父视图里就遍历子视图
     for (UIView *subview in self.subviews) {
         //把这个坐标从父视图的坐标系转为子视图的坐标系
         CGPoint subPoint = [subview convertPoint:point fromView:self];
-        UIView *result = [subview hitTest:subPoint withEvent:event];
+        UIView *responseView = [subview hitTest:subPoint withEvent:event];
         //如果事件发生在子视图里就返回
-        if (result) {
-            return result;
+        if (responseView) {
+            return responseView;
         }
     }
     return nil;
