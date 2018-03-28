816c49a2b4c578db57d0cce6120e94d7b49f2e55
xiaohuiCoding <2384618767@qq.com>
2018-03-28 15:27:13 +0800

    think about iOS responder chain

--- OCTest/TestView/TestView.m
+++ OCTest/TestView/TestView.m
@@ -10,35 +10,61 @@
 
 @implementation TestView
 
-//重写hitTest:withEvent:，以达到让整个目标子视图都能响应事件，即使子视图超出了本视图的范围仍然可以响应事件
+/**
+ 
+ *  第一响应者（First responder）指的是当前接受触摸的响应者对象（通常是一个 UIView 对象），即表示当前该对象正在与用户交互，它是响应者链的开端。响应者链和事件分发的使命都是找出第一响应者。
+ 
+ *  iOS 系统检测到手指触摸 (Touch) 操作时会将其打包成一个 UIEvent 对象，并放入当前活动 Application 的事件队列，单例的 UIApplication 会从事件队列中取出触摸事件并传递给单例的 UIWindow 来处理，UIWindow 对象首先会使用 hitTest:withEvent:方法寻找此次 Touch 操作初始点所在的视图(View)，即需要将触摸事件传递给其处理的视图，这个过程称之为 hit-test view。
+ 
+ *  hitTest:withEvent:方法的处理流程如下:
+    首先调用当前视图的 pointInside:withEvent: 方法判断触摸点是否在当前视图内；
+    若返回 NO, 则 hitTest:withEvent: 返回 nil，若返回 YES, 则向当前视图的所有子视图 (subviews) 发送 hitTest:withEvent: 消息，所有子视图的遍历顺序是从最顶层视图一直到到最底层视图，即从 subviews 数组的末尾向前遍历，直到有子视图返回非空对象或者全部子视图遍历完毕；
+    若第一次有子视图返回非空对象，则 hitTest:withEvent: 方法返回此对象，处理结束；
+    如果所有子视图都返回空，则 hitTest:withEvent: 方法返回自身 (self)。
+ 
+ */
+
+//下面是模拟系统的内部实现：
+//- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+//    UIView *touchView = self;
+//    if ([self pointInside:point withEvent:event] && !self.hidden && self.userInteractionEnabled && self.alpha >= 0.01) {
+//        for (UIView *subview in self.subviews) {
+//            CGPoint subpoint = [subview convertPoint:point fromView:self];
+//            UIView *subTouchView = [subview hitTest:subpoint withEvent:event];
+//            if (subTouchView) {
+//                touchView = subTouchView;
+//                break;
+//            }
+//        }
+//    } else {
+//        touchView = nil;
+//    }
+//    return touchView;
+//}
+
+
+
+/**
+ 
+ *  下面的代码和上面的代码的差别就是取消了pointInside:withEvent函数的检测，这样就可以捕获到当前frame范围以外的子view的触摸事件。
+ 
+ */
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
-    //排除非正常情况
-    if (!self.userInteractionEnabled || self.hidden || self.alpha <= 0.01 || self.clipsToBounds) {
-        return nil;
-    }
-    
-//    //如果事件发生在本视图内
-//    UIView *hitTestView = [super hitTest:point withEvent:event];
-//    if (hitTestView) {
-//        //如果想让本视图也能响应事件就转移响应者为本视图
-//        hitTestView = self.subviews.firstObject;
-//        return hitTestView;
-//    }
-
-    //下面的代码中如果不注释则是系统默认的实现，注释掉的部分是用来判断点击是否在父View的bounds内，如果不在父view内，就不会去其子View中寻找hitTestView，直接返回了
-    
-//    if ([self pointInside:point withEvent:event]) {
-        for (UIView *subview in [self.subviews reverseObjectEnumerator]) {
-            CGPoint subPoint = [subview convertPoint:point fromView:self];
-            UIView *hitTestView = [subview hitTest:subPoint withEvent:event];
-            if (hitTestView) {
-                return hitTestView;
+    UIView *touchView = self;
+    if (!self.hidden && self.userInteractionEnabled && self.alpha >= 0.01) {
+        for (UIView *subview in self.subviews) {
+            CGPoint subpoint = [subview convertPoint:point fromView:self];
+            UIView *subTouchView = [subview hitTest:subpoint withEvent:event];
+            if (subTouchView) {
+                touchView = subTouchView;
+                break;
             }
         }
-//        return self;
-//    }
-    return nil;
+    } else {
+        touchView = nil;
+    }
+    return touchView;
 }
 
 @end
