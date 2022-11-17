9b6ce7b9261bb18ae276eaaedab9ada1c6160270
xiaohuiCoding <3060258912@qq.com>
2022-11-17 15:34:58 +0800

    thread & runloop

--- OCTest/TestVC/TestVC17.m
+++ OCTest/TestVC/TestVC17.m
@@ -20,6 +20,7 @@
 @interface TestVC17 ()
 
 @property (nonatomic, strong) NSMutableArray *imageViews;
+@property (nonatomic, assign) BOOL needPriority;
 
 @end
 
@@ -28,7 +29,132 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+
     [self layoutUI];
+    
+    
+    // 1.任务的管理方式：串行队列 和 并行队列
+    // 2.任务的执行方式：同步 和 异步
+//    [self taskExecute];
+//    [self taskExecute2];
+//    [self taskExecute3];
+//    [self taskExecute4];
+//    [self taskExecute5];
+//    [self taskExecute6];
+//    [self taskExecute7];
+    [self taskExecute8];
+}
+
+// 打印 1 后发生死锁。。。
+- (void)taskExecute {
+    NSLog(@"1========%@",[NSThread currentThread]);
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    NSLog(@"3========%@",[NSThread currentThread]);
+}
+
+// 打印 1 2 3
+- (void)taskExecute2 {
+    NSLog(@"1========%@",[NSThread currentThread]);
+    dispatch_sync(dispatch_get_global_queue(0, 0), ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    NSLog(@"3========%@",[NSThread currentThread]);
+    // 若继续追加同步任务仍然会死锁
+//    dispatch_sync(dispatch_get_main_queue(), ^{
+//        NSLog(@"2========%@",[NSThread currentThread]);
+//    });
+}
+
+// 打印 1 3 2
+- (void)taskExecute3 {
+    NSLog(@"1========%@",[NSThread currentThread]);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    NSLog(@"3========%@",[NSThread currentThread]);
+//    NSLog(@"4========%@",[NSThread currentThread]);
+//    NSLog(@"5========%@",[NSThread currentThread]);
+    // 不管任务3后面有多少个同类型的任务，都会全部执行完后再执行任务2，因为任务2是异步执行，所以dispatch函数不会等到Block执行完成才返回，dispatch函数返回后，那任务A可以继续执行，Block任务我们可以认为在下一帧顺序加入队列，并且默认无限下一帧执行！
+}
+
+// 没有打印任何值，直接死锁
+- (void)taskExecute4 {
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSLog(@"1========%@",[NSThread currentThread]);
+    });
+//    NSLog(@"11========%@",[NSThread currentThread]);
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSLog(@"3========%@",[NSThread currentThread]);
+    });
+}
+
+// 打印 1 2 3 4 全部都在当前线程顺序执行，也就是说，同步执行不具备开辟新线程的能力。
+- (void)taskExecute5 {
+    // 串行队列 + 同步
+    dispatch_queue_t serialQueue = dispatch_queue_create("serialQueue.xiaohui.com", DISPATCH_QUEUE_SERIAL);
+    dispatch_sync(serialQueue, ^{
+        NSLog(@"1========%@",[NSThread currentThread]);
+    });
+    dispatch_sync(serialQueue, ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    dispatch_sync(serialQueue, ^{
+        NSLog(@"3========%@",[NSThread currentThread]);
+    });
+    NSLog(@"4========%@",[NSThread currentThread]);
+}
+
+// 打印 4 1 2 3 说明异步执行具有开辟新线程的能力，并且串行队列必须等到前一个任务执行完才能开始执行下一个任务，同时，异步执行会使内部函数率先返回，不会与正在执行的外部函数发生死锁。
+- (void)taskExecute6 {
+    // 串行队列 + 异步
+    dispatch_queue_t serialQueue = dispatch_queue_create("serialQueue.xiaohui.com", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(serialQueue, ^{
+        NSLog(@"1========%@",[NSThread currentThread]);
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"3========%@",[NSThread currentThread]);
+    });
+    NSLog(@"4========%@",[NSThread currentThread]);
+}
+
+// 打印 1 2 3 4 未开启新的线程执行任务，并且Block函数执行完成后dispatch函数才会返回，才能继续向下执行，所以我们看到的结果是顺序打印的。
+- (void)taskExecute7 {
+    // 并行队列 + 同步
+    dispatch_queue_t concurrentQueue = dispatch_queue_create("concurrentQueue.xiaohui.com", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_sync(concurrentQueue, ^{
+        NSLog(@"1========%@",[NSThread currentThread]);
+    });
+    dispatch_sync(concurrentQueue, ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    dispatch_sync(concurrentQueue, ^{
+        NSLog(@"3========%@",[NSThread currentThread]);
+    });
+    NSLog(@"4========%@",[NSThread currentThread]);
+}
+
+// 打印结果是随机的 开辟了多个线程，触发任务的时机是顺序的，但是我们看到完成任务的时间却是随机的，这取决于CPU对于不同线程的调度分配，但是，线程不是无条件无限开辟的，当任务量足够大时，线程是会重复利用的。
+- (void)taskExecute8 {
+    // 并行队列 + 异步
+    dispatch_queue_t concurrentQueue = dispatch_queue_create("concurrentQueue.xiaohui.com", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"1========%@",[NSThread currentThread]);
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"2========%@",[NSThread currentThread]);
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"3========%@",[NSThread currentThread]);
+    });
+    NSLog(@"4========%@",[NSThread currentThread]);
 }
 
 - (void)layoutUI {
@@ -43,23 +169,36 @@
     }
     
     UIButton *button=[UIButton buttonWithType:UIButtonTypeRoundedRect];
-    button.frame=CGRectMake(50, 660, 320, 25);
-    [button setTitle:@"加载图片" forState:UIControlStateNormal];
+    button.frame=CGRectMake(20, 650, self.view.bounds.size.width-40, 30);
+    button.backgroundColor = UIColor.blackColor;
+    [button setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    [button setTitle:@"多个线程任务顺序执行" forState:UIControlStateNormal];
     [button addTarget:self action:@selector(loadImageWithMultiThread) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
+    
+    UIButton *button2=[UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button2.frame=CGRectMake(20, 690, self.view.bounds.size.width-40, 30);
+    button2.backgroundColor = UIColor.blackColor;
+    [button2 setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    [button2 setTitle:@"多个线程任务顺序执行，指定最后一个优先执行" forState:UIControlStateNormal];
+    [button2 addTarget:self action:@selector(loadImageWithMultiThread2) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button2];
 }
 
+//创建多个线程用来加载多张图片，并按线程被创建时的顺序启动线程
 - (void)loadImageWithMultiThread {
-    //创建多个线程用来加载多张图片，并按线程被创建时的顺序启动线程
-//    NSInteger count = ROW_COUNT*COLUMN_COUNT;
-//    for (NSInteger i=0; i<count; ++i) {
-//        NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(loadImageWithIndex:) object:[NSNumber numberWithInteger:i]];
-//        thread.name = [NSString stringWithFormat:@"new thread:%li",(long)i];
-//        [thread start];
-//    }
-    
-    
-    //创建多个线程用来加载多张图片，并设置最后一个线程优先启动
+    self.needPriority = NO;
+    NSInteger count = ROW_COUNT*COLUMN_COUNT;
+    for (NSInteger i=0; i<count; ++i) {
+        NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(loadImageWithIndex:) object:[NSNumber numberWithInteger:i]];
+        thread.name = [NSString stringWithFormat:@"new thread:%li",(long)i];
+        [thread start];
+    }
+}
+
+//创建多个线程用来加载多张图片，并设置最后一个线程优先启动
+- (void)loadImageWithMultiThread2 {
+    self.needPriority = YES;
     NSInteger count = ROW_COUNT*COLUMN_COUNT;
     //创建多个线程用来加载多张图片
     for (NSInteger i = 0; i < count; ++i) {
@@ -78,7 +217,7 @@
     //参数index便是创建线程时传入的object
     NSLog(@"current thread%@",[NSThread currentThread]);//打印当前线程的编号number和名称name
     NSInteger i = [index integerValue];
-    NSLog(@"execute%ld",i);//执行顺序未必和启动顺序一致,因为线程启动后仅仅处于就绪状态，实际是否执行要由CPU根据当前状态来调度。
+    NSLog(@"execute%ld",i);//执行顺序未必和启动顺序一致，因为线程启动后仅仅处于就绪状态，实际是否执行要由CPU根据当前状态来调度。
     NSLog(@"main thread%@",[NSThread mainThread]);//主线程的number永远是1
     NSData *data = [self requestDataWithIndex:i];
     ImageData *imageData = [[ImageData alloc] init];
@@ -91,9 +230,11 @@
 //    NSURL *url = [NSURL URLWithString:@"http://g.hiphotos.baidu.com/image/pic/item/472309f790529822c4ac8ad0d5ca7bcb0a46d402.jpg"];
 //    NSData *data = [NSData dataWithContentsOfURL:url];
     
-    //为了更好的解决优先加载最后一张图片的问题，不妨让其他线程先休眠一会儿，等等最后一个线程，你将会看到最后一张图片总是第一个加载（除非网速特别差）。
-    if (index != ROW_COUNT*COLUMN_COUNT - 1) {
-        [NSThread sleepForTimeInterval:2.0];
+    if (self.needPriority == YES) {
+        //为了更好的解决优先加载最后一张图片的问题，不妨让其他线程先休眠一会儿，等等最后一个线程，你将会看到最后一张图片总是第一个加载（除非网速特别差）。
+        if (index != ROW_COUNT*COLUMN_COUNT - 1) {
+            [NSThread sleepForTimeInterval:2.0];
+        }
     }
     NSURL *url = [NSURL URLWithString:@"http://g.hiphotos.baidu.com/image/pic/item/472309f790529822c4ac8ad0d5ca7bcb0a46d402.jpg"];
     NSData *data = [NSData dataWithContentsOfURL:url];
--- OCTest/TestVC/TestVC9.m
+++ OCTest/TestVC/TestVC9.m
@@ -10,6 +10,7 @@
 
 /*
  参考：https://www.jianshu.com/p/ac05ac8428ac
+      https://segmentfault.com/a/1190000023613543
  */
 
 #import "TestVC9.h"
@@ -44,36 +45,72 @@
     //    [_imgView performSelector:@selector(setImage:) withObject:[UIImage imageNamed:@"fish"] afterDelay:2.0 inModes:@[NSRunLoopCommonModes]];
     
     NSLog(@"%@",[NSRunLoop currentRunLoop]);
-    
-    /*
-     1.CFRunLoopRef就是RunLoop，而SourceRef、TimerRef、ObserverRef是CFRunLoopRef的内容，而ModeRef指的是mode的属性。
-     
-     CFRunLoopRef
-     CFRunLoopModeRef
-     CFRunLoopSourceRef
-     CFRunLoopTimerRef
-     CFRunLoopObserverRef
-     
-     
-     2.CFRunLoopObserverRef，是RunLoop的监听者，监听的状态如下：
-
-     typedef CF_OPTIONS(CFOptionFlags, CFRunLoopActivity) {
-     kCFRunLoopEntry = (1UL << 0), //即将进入Runloop
-     kCFRunLoopBeforeTimers = (1UL << 1), //即将处理NSTimer
-     kCFRunLoopBeforeSources = (1UL << 2), //即将处理Sources
-     kCFRunLoopBeforeWaiting = (1UL << 5), //即将进入休眠
-     kCFRunLoopAfterWaiting = (1UL << 6), //刚从休眠中唤醒
-     kCFRunLoopExit = (1UL << 7), //即将退出runloop
-     kCFRunLoopAllActivities = 0x0FFFFFFFU //所有状态改变};
-     
-     3.
-     kCFRunLoopDefaultMode //App的默认Mode，通常主线程是在这个Mode下运行
-     UITrackingRunLoopMode //界面跟踪 Mode，用于 ScrollView 追踪触摸滑动，保证界面滑动时不受其他 Mode 影响
-     UIInitializationRunLoopMode // 在刚启动 App 时第进入的第一个 Mode，启动完成后就不再使用
-     GSEventReceiveRunLoopMode // 接受系统事件的内部 Mode，通常用不到
-     kCFRunLoopCommonModes //这是一个占位用的Mode，不是一种真正的Mode
-
-     */
 }
+
+/*
+ 一、runloop 的基本概念
+ 
+ 1.常说的 runloop 其实就是 CFRunLoopRef，它的内容：一个 runloop 可以包含多个 Mode，一个 Mode 可以包含多个 source timer observer，每次调用 runLoop 的主函数时，只能指定其中一个 Mode，这个Mode被称作 CurrentMode。如果需要切换 Mode，只能退出 Loop，再重新指定一个 Mode 进入。这样做主要是为了分隔开不同组的 Source/Timer/Observer，让其互不影响。
+ CFRunLoopModeRef
+ CFRunLoopSourceRef
+ CFRunLoopTimerRef
+ CFRunLoopObserverRef
+ 
+ 2.CFRunLoopObserverRef 监听的几种状态
+ typedef CF_OPTIONS(CFOptionFlags, CFRunLoopActivity) {
+ kCFRunLoopEntry = (1UL << 0), //即将进入Runloop
+ kCFRunLoopBeforeTimers = (1UL << 1), //即将处理NSTimer
+ kCFRunLoopBeforeSources = (1UL << 2), //即将处理Sources
+ kCFRunLoopBeforeWaiting = (1UL << 5), //即将进入休眠
+ kCFRunLoopAfterWaiting = (1UL << 6), //刚从休眠中唤醒
+ kCFRunLoopExit = (1UL << 7), //即将退出runloop
+ kCFRunLoopAllActivities = 0x0FFFFFFFU //所有状态改变};
+ 
+ 3.几种 Mode 的意义
+ kCFRunLoopDefaultMode //App的默认Mode，通常主线程是在这个Mode下运行
+ UITrackingRunLoopMode //界面跟踪 Mode，用于 ScrollView 追踪触摸滑动，保证界面滑动时不受其他 Mode 影响
+ UIInitializationRunLoopMode // 在刚启动 App 时第进入的第一个 Mode，启动完成后就不再使用
+ GSEventReceiveRunLoopMode // 接受系统事件的内部 Mode，通常用不到
+ kCFRunLoopCommonModes //这是一个占位用的Mode，不是一种真正的Mode
+ */
+
+/*
+ 二、线程与runloop的关系是一一对应的，源码解释如下：
+ 
+ 1.全局的字典是 loopsDic，key 是 thread， value 是其对应的 runloop;
+ 2.访问 loopsDic 时的锁是 static CFSpinLock_t loopsLock;
+ 3.获取一个 thread 对应的 runLoop;
+
+ // 获取一个线程的 runloop
+ CFRunLoopRef _CFRunLoopGet(pthread_t thread) {
+     CFRunLoopRefstatic CFMutableDictionaryRef loopsDic; // 声明一个全局的字典，用来存放 thread 和其对应的 loop
+     static CFSpinLock_t loopsLock; // 声明一个锁
+     OSSpinLockLock(&loopsLock); // 加锁
+     if (!loopsDic) {
+         loopsDic = CFDictionaryCreateMutable(); // 初始化全局的 loopsDic
+         CFRunLoopRef mainLoop = _CFRunLoopCreate(); // 先给主线程创建一个 mainLoop
+         CFDictionarySetValue(loopsDic, pthread_main_thread_np(), mainLoop); // 为全局的 loopsDic 添加主线程的 key value
+     }
+     CFRunLoopRef loop = CFDictionaryGetValue(loopsDic, thread)); // 默认直接从全局的 loopsDic 里获取
+     if (!loop) {
+         loop = _CFRunLoopCreate(); // 如果取不到就创建一个 runloop
+         CFDictionarySetValue(loopsDic, thread, loop); // 为全局的 loopsDic 添加该线程的 key value
+         _CFSetTSD(..., thread, loop, __CFFinalizeRunLoop); // 注册一个回调，当线程销毁时，顺便也销毁其对应的 runLoop
+     }
+     OSSpinLockUnLock(&loopsLock); // 解锁
+     return loop;
+ }
+ 
+ // 获取主线程的 runloop
+ CFRunLoopRef CFRunLoopGetMain() {
+     return _CFRunLoopGet(pthread_main_thread_np());
+ }
+ 
+ // 获取当前线程的 runloop
+ CFRunLoopRef CFRunLoopGetCurrent() {
+     return _CFRunLoopGet(pthread_self());
+ }
+    
+ */
 
 @end
